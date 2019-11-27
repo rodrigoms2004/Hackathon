81938072a8f8bc8e4582e5f04cbc4a446bc101ab
Alexandre Preczewski <apreczewski@gmail.com>
2019-11-27 09:48:37 -0300

    game views

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Routes from './routes';
 import GlobalStyle from './styles/global';
+import {Router } from 'react-router-dom';
+import history from './services/history'
 
 function App() {
   return (
-    <>
+    <Router history={history}>
       <Routes />
       <GlobalStyle />
-    </>
+    </Router>
   );
 }
 
--- front-end/src/pages/Login/index.js
+++ front-end/src/pages/Login/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { Form, Input } from '@rocketseat/unform';
 import { Link } from 'react-router-dom';
 import { Container } from './styles';
+import history from '../../services/history';
 
 export default function Login() {
+
+  function handleSignin(){
+    history.push('/game')
+  }
+
   return (
     <Container>
       <h2>Rocket Challenger</h2>
-      <Form onSubmit={() => {}}>
+      <Form onSubmit={handleSignin}>
         <Input name="email" type="email" placeholder="Digite seu email" />
         <Input name="password" type="password" placeholder="Digite sua senha" />
         <button type="submit">Entrar</button>
