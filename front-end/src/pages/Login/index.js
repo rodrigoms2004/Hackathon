88ec86f51ea7b6a9bef1b68e1cc6b943a4bb0627
Rodrigo Moraes Silveira <rodrigoms2004@gmail.com>
2019-11-27 10:57:08 -0300

    service API login e registro

--- front-end/src/pages/Login/index.js
+++ front-end/src/pages/Login/index.js
@@ -1,16 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Input } from '@rocketseat/unform';
 import { Link } from 'react-router-dom';
 import { Container } from './styles';
+import api from '../../services/api';
+
 
 export default function Login() {
+
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  function postLogin() {
+    api.post("/login", {
+      username,
+      password
+    }).then(result => {
+      if (result.status === 200) {
+        console.log(result.data);
+      } else {
+        console.log("error");
+      }
+    }).catch(e => {
+      console.log("error:", e)
+    });
+  }
+
   return (
     <Container>
       <h2>Rocket Challenger</h2>
-      <Form onSubmit={() => {}}>
-        <Input name="email" type="email" placeholder="Digite seu email" />
-        <Input name="password" type="password" placeholder="Digite sua senha" />
-        <button type="submit">Entrar</button>
+      <Form onSubmit={postLogin}>
+        <Input 
+          name="username" 
+          type="username" 
+          value={username} 
+          onChange={e => {
+          setUserName(e.target.value)
+         }} 
+         placeholder="Digite seu nome de usuário" 
+        />
+        <Input 
+          name="password" 
+          type="password" 
+          value={password}
+          onChange={e => {
+            setPassword(e.target.value)
+          }}
+          placeholder="Digite sua senha" 
+        />
+        <button 
+          type="submit">Entrar
+        </button>
       </Form>
       <Link to="/register">Cadastre-se</Link>
     </Container>
--- front-end/src/pages/Register/index.js
+++ front-end/src/pages/Register/index.js
@@ -1,17 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Input } from '@rocketseat/unform';
 import { Link } from 'react-router-dom';
 import { Container } from './styles';
+import api from '../../services/api';
 
 export default function Register() {
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+
+  function register() {
+    api.post("/register", {
+      username,
+      password,
+      email,
+      profile: 2,
+      name
+    }).then(result => {
+      if (result.status === 200) {
+        console.log(result.data);
+      } else {
+        console.log("error");
+      }
+    }).catch(e => {
+      console.log("error:", e)
+    });
+  }
+
   return (
     <Container>
       <h2>Rocket Challenger</h2>
-      <Form onSubmit={() => {}}>
-        <Input name="name" type="text" placeholder="Digite seu nome" />
-        <Input name="telefone" type="text" placeholder="Digite seu telefone" />
-        <Input name="email" type="email" placeholder="Digite seu email" />
-        <Input name="password" type="password" placeholder="Digite sua senha" />
+      <Form onSubmit={register}>
+        <Input name="name" type="text" 
+          value={name} 
+          onChange={e => {
+            setName(e.target.value)
+          }} 
+          placeholder="Digite seu nome" 
+        />
+
+        <Input name="username" type="username" 
+          value={username}
+          onChange={e => {
+            setUserName(e.target.value)
+          }}
+          placeholder="Digite seu nome de usuário" 
+        />
+
+        <Input name="email" type="email" 
+          value={email}
+          onChange={e => {
+            setEmail(e.target.value)
+          }}
+          placeholder="Digite seu email" 
+        />
+        <Input name="password" type="password" 
+          value={password}
+          onChange={e => {
+            setPassword(e.target.value)
+          }}
+          placeholder="Digite sua senha" 
+        />
         <button type="submit">Cadastrar</button>
       </Form>
       <Link to="/">Já possuo conta</Link>
